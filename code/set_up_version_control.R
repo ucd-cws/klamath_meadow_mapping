59bdd8896c076a2f1ee3d0609e2cb19fa8c5155b
Ryan Peek <rapeek@ucdavis.edu>
2019-10-28 14:29:42 -0700

    add new data and gitignore

---
+++ code/set_up_version_control.R
@@ -0,0 +1,15 @@
+# make project version controlled
+# see https://happygitwithr.com/existing-github-last.html
+
+library(usethis)
+
+# set up Git in Existing Project
+usethis::use_git()
+
+# now create a new Github Repo on web (best for CWS account), or 
+
+# To set up in Personal Github Page:
+# usethis::use_github()
+
+# then in shell/Terminal:
+# git remote add origin git@github.com:ucd-cws/klamath_meadow_mapping.git
