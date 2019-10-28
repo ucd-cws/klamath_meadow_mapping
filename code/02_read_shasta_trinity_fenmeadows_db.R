5fdbe130a2aed5c0a788b2453b087852d819cb04
Ryan Peek <rapeek@ucdavis.edu>
2019-10-28 14:44:21 -0700

    connect to mdb with tables

---
+++ code/02_read_shasta_trinity_fenmeadows_db.R
@@ -0,0 +1,34 @@
+# 02_read_mdw_database
+
+library(Hmisc)
+library(tidyverse)
+library(fs)
+library(here)
+library(RSQLite)
+
+# get database path:
+mdblink <- as_fs_path(paste0(here(), "/data/Fen_Meadows_Shasta_Trinity.mdb"))
+
+# see table names:
+(mDB <- mdb.get(mdblink, tables=TRUE))
+
+# get single table
+features <- mdb.get(mdblink, tables="Features", stringsAsFactors=F) 
+
+# drop the "attributes" component that gets added to the dataframe (annoying but won't cause trouble)
+
+# Here's a function to remove the attrs:
+clear.labels <- function(x) {
+  if(is.list(x)) {
+    for(i in 1 : length(x)) class(x[[i]]) <- setdiff(class(x[[i]]), 'labelled') 
+    for(i in 1 : length(x)) attr(x[[i]],"label") <- NULL
+  }
+  else {
+    class(x) <- setdiff(class(x), "labelled")
+    attr(x, "label") <- NULL
+  }
+  return(x)
+}
+
+# now run the function
+features<- clear.labels(features)
